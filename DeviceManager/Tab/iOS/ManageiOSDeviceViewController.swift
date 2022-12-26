592bb685cac5e3c2c24fae293b002e3670c6693a
Rin-t <rin.t.4921@gmail.com>
2022-12-26 15:20:56 +0900

    iOS端末の管理画面追加

---
+++ DeviceManager/Tab/iOS/ManageiOSDeviceViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ManageiOSDeviceViewController.swift
+//  DeviceManager
+//
+//  Created by Rin on 2022/12/25.
+//
+
+import UIKit
+
+final class ManageiOSDeviceViewController: UIViewController {
+    
+}

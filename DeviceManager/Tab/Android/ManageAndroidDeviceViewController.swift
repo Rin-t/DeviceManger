62e189258071e6229f18f085554ad441ac5d4712
Rin-t <rin.t.4921@gmail.com>
2022-12-26 15:20:43 +0900

    Android端末の管理ViewController追加

---
+++ DeviceManager/Tab/Android/ManageAndroidDeviceViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ManageAndroidDeviceViewContrller.swift
+//  DeviceManager
+//
+//  Created by Rin on 2022/12/25.
+//
+
+import UIKit
+
+final class ManageAndroidDeviceViewController: UIViewController {
+
+}

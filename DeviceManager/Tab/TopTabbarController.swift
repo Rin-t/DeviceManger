9faf0b829dab43ba7b1f5f7d122ac2e030ee250b
Rin-t <rin.t.4921@gmail.com>
2022-12-26 15:20:16 +0900

    TabControllerの追加

---
+++ DeviceManager/Tab/TopTabbarController.swift
@@ -0,0 +1,61 @@
+//
+//  TopTabbarController.swift
+//  DeviceManager
+//
+//  Created by Rin on 2022/12/25.
+//
+
+
+import UIKit
+
+final class TopTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tabBar.backgroundColor = .clear
+        UITabBar.appearance().tintColor = .black
+        UITabBar.appearance().backgroundColor = .systemGray5
+
+        setViewControllers([
+            setupWriteMemoViewController(),
+            setupMenuViewController()
+        ], animated: false)
+    }
+
+    private func setupWriteMemoViewController() -> UIViewController {
+        let vc = ManageiOSDeviceViewController()
+        let nav = UINavigationController(rootViewController: vc)
+        nav.tabBarItem = .init(title: "iOS", image: UIImage(systemName: "applelogo"), tag: 0)
+        nav.navigationItem.title = "iOS"
+        return nav
+    }
+
+    private func setupMenuViewController() -> UIViewController {
+        let vc = ManageAndroidDeviceViewController()
+        let nav = UINavigationController(rootViewController: vc)
+        let image = UIImage(named: "android")
+        let rewSizeImage = image?.resize(size: CGSize(width: 24, height: 24))
+        nav.tabBarItem = .init(title: "Android", image: rewSizeImage, tag: 1)
+        nav.title = "Android"
+        return nav
+    }
+    
+}
+
+extension UIImage {
+    func resize(size _size: CGSize) -> UIImage? {
+        let widthRatio = _size.width / size.width
+        let heightRatio = _size.height / size.height
+        let ratio = widthRatio < heightRatio ? widthRatio : heightRatio
+
+        let resizedSize = CGSize(width: size.width * ratio, height: size.height * ratio)
+
+        UIGraphicsBeginImageContextWithOptions(resizedSize, false, 0.0) // 変更
+        draw(in: CGRect(origin: .zero, size: resizedSize))
+        let resizedImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+
+        return resizedImage
+    }
+}

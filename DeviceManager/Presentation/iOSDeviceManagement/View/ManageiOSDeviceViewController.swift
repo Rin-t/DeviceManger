a58e1d19b3bdbb8f9586fab150ed6b7c9cbd04f2
Rin-t <rin.t.4921@gmail.com>
2023-01-03 16:53:33 +0900

    format: フォルダの整理

--- DeviceManager/Presentation/iOSDeviceManagement/View/ManageiOSDeviceViewController.swift
+++ DeviceManager/Presentation/iOSDeviceManagement/View/ManageiOSDeviceViewController.swift
@@ -8,5 +8,10 @@
 import UIKit
 
 final class ManageiOSDeviceViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemGray5
+    }
     
 }

3511614e294ff5e07c2f053d726573bf8f4b7a59
Rin-t <rin.t.4921@gmail.com>
2023-01-07 21:00:38 +0900

    fix: データクラスの移動

---
+++ DeviceManager/Data/Entity/DeviceDataEntity.swift
@@ -0,0 +1,16 @@
+//
+//  DeviceDataEntity.swift
+//  DeviceManager
+//
+//  Created by Rin on 2023/01/07.
+//
+
+import Foundation
+
+struct DeviceDataEntity {
+    let device: String
+    let holder: String
+    let version: String
+    let color: String
+    let accountName: String
+}

2c5494b36eda53f108422015625ef2353a27e2b5
Rin-t <rin.t.4921@gmail.com>
2023-01-08 22:33:51 +0900

    add: UserdefaultのExtensionを追加

---
+++ DeviceManager/Extensions/UserDefault+Extension.swift
@@ -0,0 +1,42 @@
+//
+//  UserDefault+Extension.swift
+//  DeviceManager
+//
+//  Created by Rin on 2023/01/08.
+//
+
+import Foundation
+
+extension UserDefaults {
+
+    enum StructDataError: Error {
+        case encodeError
+        case convertToDataError
+        case decodeError
+    }
+
+    func set<T: Encodable>(struct data: T, key: String) throws {
+        do {
+            let encoder = JSONEncoder()
+            encoder.keyEncodingStrategy = .convertToSnakeCase
+            let data = try encoder.encode(data)
+            UserDefaults.standard.set(data, forKey: key)
+        } catch {
+            throw StructDataError.encodeError
+        }
+    }
+
+    func structData<T: Decodable>(key: String) throws -> T {
+        do {
+            let decoder = JSONDecoder()
+            decoder.keyDecodingStrategy = .convertFromSnakeCase
+            guard let data = UserDefaults.standard.data(forKey: key) else {
+                throw StructDataError.convertToDataError
+            }
+            let decodedData = try decoder.decode(T.self, from: data)
+            return decodedData
+        } catch {
+            throw StructDataError.decodeError
+        }
+    }
+}

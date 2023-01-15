541b2707ae98bc31477e1458eeb7c2327db13c40
Rin-t <rin.t.4921@gmail.com>
2023-01-15 16:35:59 +0900

    add: デバイス情報の編集画面を追加

---
+++ DeviceManager/Presentation/Common/EditDeviceData/View/EditDeviceDataViewController.swift
@@ -0,0 +1,14 @@
+//
+//  EditDeviceDataViewController.swift
+//  DeviceManager
+//
+//  Created by Rin on 2023/01/15.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+final class EditDeviceDataViewController: UIViewController {
+
+}
---
+++ DeviceManager/Presentation/Common/EditDeviceData/ViewModel/EditDeviceDataViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  EditDeviceDataViewModel.swift
+//  DeviceManager
+//
+//  Created by Rin on 2023/01/15.
+//
+
+import Foundation
+import RxSwift
+import RxRelay
+
+final class EditDeviceDataViewModel {
+    
+}

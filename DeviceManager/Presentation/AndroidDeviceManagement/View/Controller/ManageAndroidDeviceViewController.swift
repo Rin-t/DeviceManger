12f7b1e2dda275466c8e8fbfc98439fa9137027b
Rin-t <rin.t.4921@gmail.com>
2023-01-16 22:35:08 +0900

    update: Androidのデバイス管理画面を作成

--- DeviceManager/Presentation/AndroidDeviceManagement/View/Controller/ManageAndroidDeviceViewController.swift
+++ DeviceManager/Presentation/AndroidDeviceManagement/View/Controller/ManageAndroidDeviceViewController.swift
@@ -6,12 +6,84 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 final class ManageAndroidDeviceViewController: UIViewController {
+
+    typealias Cell = DeviceManagementCollectionViewCell
+
     @IBOutlet private weak var collectionView: UICollectionView! {
         didSet {
             collectionView.layer.cornerRadius = 10
+            collectionView.register(Cell.nib(), forCellWithReuseIdentifier: Cell.identifier)
+            collectionView.showsVerticalScrollIndicator = false
         }
+    }
+
+    @IBOutlet private weak var addDeviceButton: UIButton! {
+        didSet {
+            addDeviceButton.layer.cornerRadius = addDeviceButton.frame.width / 2
+        }
+    }
+    private let disposeBag = DisposeBag()
+    private let viewModel = ManageAndroidDeviceViewModel(
+        useCase: DeviceDataUseCase(
+            dataStore: DeviceDataStore(type: .android)
+        )
+    )
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+        setupCollectionViewLayout()
+        setupBindings()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        viewModel.input.viewWillAppear()
+        tabBarController?.tabBar.isHidden = false
+    }
+
+    private func setupBindings() {
+        viewModel.output.deviceData
+            .bind(to: collectionView.rx.items) { (collectionView, row, element) in
+                let indexPath = IndexPath(row: row, section: 0)
+                let cell = collectionView.dequeueReusableCell(withReuseIdentifier: Cell.identifier, for: indexPath) as! Cell
+                cell.configure(data: element)
+                return cell
+            }
+            .disposed(by: disposeBag)
+
+        collectionView.rx.itemSelected
+            .subscribe(onNext: { [weak self] indexPath in
+                self?.collectionView.deselectItem(at: indexPath, animated: true)
+                let vc = EditDeviceDataViewController(type: .android, selectedIndex: indexPath)
+                self?.navigationController?.pushViewController(vc, animated: true)
+            })
+            .disposed(by: disposeBag)
+
+        addDeviceButton.rx.tap
+            .subscribe(onNext: { [weak self] _ in
+                let vc = AddNewDeviceDataViewController(type: .android)
+                self?.navigationController?.pushViewController(vc, animated: true)
+            })
+            .disposed(by: disposeBag)
+    }
+
+    private func setupCollectionViewLayout() {
+        let cellWidth = collectionView.frame.width - 32
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(width: cellWidth, height: 120)
+        layout.sectionInset = UIEdgeInsets(top: 16, left: 16, bottom: 80, right: 16)
+        layout.minimumLineSpacing = 32
+        collectionView.collectionViewLayout = layout
+    }
+
+    private func setupView() {
+        view.backgroundColor = .systemGray5
+        navigationItem.title = "Androidデバイス管理"
     }
 
 }

8403fae641624e5a3d79f8e1482aba19a9dab5cb
Rin-t <rin.t.4921@gmail.com>
2023-01-16 22:38:38 +0900

    fixed: iOSのデバイス管理画面の修正

--- DeviceManager/Presentation/iOSDeviceManagement/View/Controller/ManageiOSDeviceViewController.swift
+++ DeviceManager/Presentation/iOSDeviceManagement/View/Controller/ManageiOSDeviceViewController.swift
@@ -27,32 +27,63 @@ final class ManageiOSDeviceViewController: UIViewController {
         }
     }
     private let disposeBag = DisposeBag()
-    private let viewModel = ManageiOSDeviceViewModel(useCase: DeviceDataUseCase(dataStore: DeviceDataStore(type: .iOS)))
+    private let viewModel = ManageiOSDeviceViewModel(
+        useCase: DeviceDataUseCase(
+            dataStore: DeviceDataStore(type: .iOS)
+        )
+    )
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemGray5
-        navigationItem.title = "iOSデバイス管理"
-
-        setupCollectionLayout()
-        viewModel.input.viewWillAppear()
-        viewModel.deviceData.bind(to: collectionView.rx.items) { (collectionView, row, element) in
-            print(element)
-            let indexPath = IndexPath(row: row, section: 0)
-            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: Cell.identifier, for: indexPath) as! Cell
-            cell.configure()
-            return cell
-        }
-        .disposed(by: disposeBag)
+        setupView()
+        setupCollectionViewLayout()
+        setupBindings()
     }
 
-    private func setupCollectionLayout() {
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        viewModel.input.viewWillAppear()
+        tabBarController?.tabBar.isHidden = false
+    }
+
+    private func setupBindings() {
+        viewModel.output.deviceData
+            .bind(to: collectionView.rx.items) { (collectionView, row, element) in
+                let indexPath = IndexPath(row: row, section: 0)
+                let cell = collectionView.dequeueReusableCell(withReuseIdentifier: Cell.identifier, for: indexPath) as! Cell
+                cell.configure(data: element)
+                return cell
+            }
+            .disposed(by: disposeBag)
+
+        collectionView.rx.itemSelected
+            .subscribe(onNext: { [weak self] indexPath in
+                self?.collectionView.deselectItem(at: indexPath, animated: true)
+                let vc = EditDeviceDataViewController(type: .iOS, selectedIndex: indexPath)
+                self?.navigationController?.pushViewController(vc, animated: true)
+            })
+            .disposed(by: disposeBag)
+
+        addDeviceButton.rx.tap
+            .subscribe(onNext: { [weak self] _ in
+                let vc = AddNewDeviceDataViewController(type: .iOS)
+                self?.navigationController?.pushViewController(vc, animated: true)
+            })
+            .disposed(by: disposeBag)
+    }
+
+    private func setupCollectionViewLayout() {
         let cellWidth = collectionView.frame.width - 32
         let layout = UICollectionViewFlowLayout()
         layout.itemSize = CGSize(width: cellWidth, height: 120)
         layout.sectionInset = UIEdgeInsets(top: 16, left: 16, bottom: 80, right: 16)
         layout.minimumLineSpacing = 32
         collectionView.collectionViewLayout = layout
+    }
+
+    private func setupView() {
+        view.backgroundColor = .systemGray5
+        navigationItem.title = "iOSデバイス管理"
     }
 }
 
